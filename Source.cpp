2c0e8a6241b949802415162868f828b712ae4b3b
Andrey <andron152d@gmail.com>
2024-06-26 15:41:39 +0300

    second

--- Source.cpp
+++ Source.cpp
@@ -12,19 +12,25 @@ struct Employee {
 };
 
 int main() {
-    Employee emp;
+  
+    Employee employees[5];
 
-    emp.surname = "Іваненко";
-    emp.birthYear = 1985;
-    emp.position = "Інженер";
-    emp.salary = 25000.50;
-    emp.education = "Вища";
 
-    cout << "Прізвище: " << emp.surname << endl;
-    cout << "Рік народження: " << emp.birthYear << endl;
-    cout << "Посада: " << emp.position << endl;
-    cout << "Зарплата: " << emp.salary << endl;
-    cout << "Освіта: " << emp.education << endl;
+    employees[0] = { "Іваненко", 1985, "Інженер", 25000.50, "Вища" };
+    employees[1] = { "Петренко", 1990, "Програміст", 30000.75, "Вища" };
+    employees[2] = { "Сидоренко", 1988, "Аналітик", 28000.00, "Вища" };
+    employees[3] = { "Коваленко", 1982, "Менеджер", 27000.25, "Вища" };
+    employees[4] = { "Михайленко", 1987, "Дизайнер", 26000.80, "Вища" };
+
+    
+    for (int i = 0; i < 5; ++i) {
+        cout << "Прізвище: " << employees[i].surname << endl;
+        cout << "Рік народження: " << employees[i].birthYear << endl;
+        cout << "Посада: " << employees[i].position << endl;
+        cout << "Зарплата: " << employees[i].salary << endl;
+        cout << "Освіта: " << employees[i].education << endl;
+        cout << endl;
+    }
 
     return 0;
 }
