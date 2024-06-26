0b18c53634f6d726a3afaaa21f46668959956617
Andrey <andron152d@gmail.com>
2024-06-26 15:43:03 +0300

    third

--- Source.cpp
+++ Source.cpp
@@ -12,25 +12,33 @@ struct Employee {
 };
 
 int main() {
-  
-    Employee employees[5];
+    setlocale(0, "");
+    const int size = 5;  // Розмір масиву працівників
+    Employee employees[size] = {
+        {"Іваненко", 1955, "Інженер", 25000.50, "Вища"},
+        {"Петренко", 1960, "Програміст", 30000.75, "Вища"},
+        {"Сидоренко", 1988, "Аналітик", 28000.00, "Вища"},
+        {"Коваленко", 1958, "Менеджер", 27000.25, "Вища"},
+        {"Михайленко", 1970, "Дизайнер", 26000.80, "Вища"}
+    };
 
+    // Визначення кількості працівників старших за 60 років і виведення їх відомостей
+    int olderThan60Count = 0;
 
-    employees[0] = { "Іваненко", 1985, "Інженер", 25000.50, "Вища" };
-    employees[1] = { "Петренко", 1990, "Програміст", 30000.75, "Вища" };
-    employees[2] = { "Сидоренко", 1988, "Аналітик", 28000.00, "Вища" };
-    employees[3] = { "Коваленко", 1982, "Менеджер", 27000.25, "Вища" };
-    employees[4] = { "Михайленко", 1987, "Дизайнер", 26000.80, "Вища" };
-
-    
-    for (int i = 0; i < 5; ++i) {
-        cout << "Прізвище: " << employees[i].surname << endl;
-        cout << "Рік народження: " << employees[i].birthYear << endl;
-        cout << "Посада: " << employees[i].position << endl;
-        cout << "Зарплата: " << employees[i].salary << endl;
-        cout << "Освіта: " << employees[i].education << endl;
-        cout << endl;
+    cout << "Працівники старші за 60 років:" << endl;
+    for (int i = 0; i < size; ++i) {
+        if (2024 - employees[i].birthYear > 60) {
+            olderThan60Count++;
+            cout << "Прізвище: " << employees[i].surname << endl;
+            cout << "Рік народження: " << employees[i].birthYear << endl;
+            cout << "Посада: " << employees[i].position << endl;
+            cout << "Зарплата: " << employees[i].salary << endl;
+            cout << "Освіта: " << employees[i].education << endl;
+            cout << endl;
+        }
     }
+
+    cout << "Всього працівників старших за 60 років: " << olderThan60Count << endl;
 
     return 0;
 }
